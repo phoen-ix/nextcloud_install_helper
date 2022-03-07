bd25ddb8d5a7ca6c7acecc4dabd5746803c0120a
phoen-ix <100175489+phoen-ix@users.noreply.github.com>
2022-03-07 19:43:32 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -361,7 +361,7 @@ fn_install_docker() {
    fn_apt_autoremove_update_upgrade
    sudo apt install docker-ce docker-ce-cli containerd.io -y
    sudo curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-   sudo chown +x /usr/local/bin/docker-compose
+   sudo chmod +x /usr/local/bin/docker-compose
    echo "adding docker iptables mod"
    echo "DOCKER_OPTS=\"--iptables=false\"" | sudo tee -a /etc/default/docker
    echo "restart docker"
